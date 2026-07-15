7f9b995bc34cf538f3cfcaa8e12b6c271d97d8db
rahulaes17 <rahulaes1704@gmail.com>
2026-07-15 15:38:13 +0530

    Learnt the way to use Tailwind CSS in react

---
+++ 06-Tailwind CSS/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App = () => {
+  return (
+    <div className="h-screen w-full bg-gray-700">
+      <h1 className="text-3xl font-bold underline text-white text-center">
+        Hello world!
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
